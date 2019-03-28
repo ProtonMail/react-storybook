7e19935e238af42105bfec621b7611bb6e816830
Richard <richard@protonmail.com>
2019-03-28 16:53:24 +0100

    Add other examples

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,9 +2,29 @@ import React from 'react';
 import 'react-components/styles/index.scss';
 import { storiesOf } from '@storybook/react';
 
-import { Button } from 'react-components';
+import { Button, PrimaryButton, LinkButton, Alert } from 'react-components';
+
+storiesOf('Alert', module)
+  .add('Info', () => (
+    <Alert>Info</Alert>
+  ))
+  .add('Warning', () => (
+    <Alert type="warning">Warning</Alert>
+  ))
+  .add('Error', () => (
+    <Alert type="error">Error</Alert>
+  ))
+  .add('with LearnMore link', () => (
+    <Alert learnMore="https://protonmail.com">Info</Alert>
+  ));
 
 storiesOf('Button', module)
-  .add('Basic Button', () => (
+  .add('Basic', () => (
     <Button>Hello Button</Button>
+  ))
+  .add('Primary button', () => (
+    <PrimaryButton>Hello Button</PrimaryButton>
+  ))
+  .add('Link button', () => (
+    <LinkButton>Hello Button</LinkButton>
   ));
