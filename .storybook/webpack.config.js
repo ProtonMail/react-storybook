27e90b8f33e91441ff316aa0f30a0b407d240a34
Richard <richard@protonmail.com>
2019-04-24 14:50:31 +0200

    Update webpack config to make npm link react-components works

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,7 @@ const assetsLoader = require('proton-pack/webpack/assets.loader');
 const cssLoader = require('proton-pack/webpack/css.loader');
 const jsLoader = require('proton-pack/webpack/js.loader');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = async ({ config, mode }) => {
     config.plugins = [
@@ -17,6 +18,19 @@ module.exports = async ({ config, mode }) => {
         ...cssLoader({}),
         ...jsLoader({})
     ];
+    config.resolve = {
+        ...config.resolve,
+        symlinks: false,
+        alias: {
+            // Ensure that the correct package is used when symlinking
+            pmcrypto: path.resolve('./node_modules/pmcrypto'),
+            react: path.resolve('./node_modules/react'),
+            'react-dom': path.resolve('./node_modules/react-dom'),
+            'design-system': path.resolve('./node_modules/design-system'),
+            'proton-shared': path.resolve('./node_modules/proton-shared'),
+            'react-components': path.resolve('./node_modules/react-components')
+        }
+    };
 
     return config;
 };
